efa4237c052067794577f54d2290ce871abe9f8e
kema1989 <86516029+kema1989@users.noreply.github.com>
2021-08-18 12:42:02 +0600

    Добавьте файлы проекта.

---
+++ Module8Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Module8Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo directory = new DirectoryInfo(@"C:\Users\YOGA\Desktop\Кема2курс\Физика3семестр");
+            try
+            {
+                if (directory.Exists)
+                {
+                    DateTime thirty = DateTime.Now - TimeSpan.FromMinutes(300);
+                    foreach (FileInfo file in directory.GetFiles())
+                    {
+                        if (file.LastAccessTime < thirty)
+                        {
+                            file.Delete();
+                        }
+                    }
+                    foreach (DirectoryInfo papka in directory.GetDirectories())
+                    {
+                        if (papka.LastAccessTime < thirty)
+                        {
+                            papka.Delete(true);
+                        }
+                    }
+                }
+                else
+                {
+                    throw new Exception("Такой папки не существует");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+    }
+}
